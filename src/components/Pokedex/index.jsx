50bda9fdadf83b0888b76b73a695a79e38b48cba
ioptyu2 <mbatta1996@hotmail.com>
2023-07-20 12:40:13 +0100

    Header and api fetch setup

--- src/components/Pokedex/index.jsx
+++ src/components/Pokedex/index.jsx
@@ -1,7 +1,31 @@
+import React, {useEffect, useState} from "react"
+import {Pokemon} from "../"
 
 
+export default function Pokedex() {
+    const [pokedex, setPokedex] = useState([])
 
-export default function Pokedex({pokedex, setPokedex}) {
-    
+    useEffect(() => {
+        async function displayPokedex() {
+            const arr = []
+            for(let i = 1; i < 100; i++) {
+                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+                const data = await response.json();
+                arr.push(data)
+            }
+            
+            setPokedex(data) 
+        }
+        displayPokedex()
+    }, [])
+    return (
+        <>
+        {pokedex.map((pokemon) => {
+           return (
+            <Pokemon data={pokedex}/>
+           ) 
+        })}
+        </>
+    )
 
 }
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,2 +1,4 @@
 export {default as Header} from './Header'
+export {default as Pokedex} from './Pokedex'
+export {default as Pokemon} from './Pokemon'
 
